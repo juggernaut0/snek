a92c41273dccfc472c66f28783439d6ab8ddad5d
Tom Warner <tomwamt@gmail.com>
2019-06-30 22:44:25 -0600

    Resolver tweaks

--- src/resolver.rs
+++ src/resolver.rs
@@ -3,11 +3,14 @@ use crate::ast::*;
 use std::rc::Rc;
 
 #[derive(Eq, PartialEq, Hash, Clone)]
-pub struct Declaration(Rc<NamePattern>);
+pub struct Declaration {
+    name: Rc<NamePattern>,
+    id: u32
+}
 
 impl Declaration {
     pub fn name(&self) -> &String {
-        &self.0.name
+        &self.name.name
     }
 }
 
@@ -31,8 +34,9 @@ impl QNameExpr {
 }
 
 pub struct Resolver {
-    declarations: HashMap<Rc<NamePattern>, Declaration>,
-    usages: HashMap<QNameExpr, Declaration>,
+    declarations: HashMap<Rc<NamePattern>, u32>,
+    usages: HashMap<QNameExpr, u32>,
+    declaration_id_seq: u32
 }
 
 impl Resolver {
@@ -40,13 +44,14 @@ impl Resolver {
         let mut r = Resolver {
             declarations: HashMap::new(),
             usages: HashMap::new(),
+            declaration_id_seq: 0
         };
         r.resolve(ast);
         r
     }
 
     fn resolve(&mut self, ast: &Ast) {
-        let declarations: Vec<_> = ast.root_namespace.decls.iter().flat_map(|d| find_declarations(d)).collect();
+        let declarations: Vec<_> = ast.root_namespace.decls.iter().flat_map(|d| self.find_declarations(d)).collect();
         let root_scope = Scope::new(&declarations, None);
         self.add_declarations(&declarations);
         for decl in &ast.root_namespace.decls {
@@ -65,26 +70,35 @@ impl Resolver {
         unimplemented!("resolve_usages_expr")
     }
 
-    fn add_declarations(&mut self, declarations: &Vec<Declaration>) {
-        for d in declarations {
-            self.declarations.insert(Rc::clone(&d.0), d.clone());
+    fn find_declarations(&mut self, decl: &Decl) -> Vec<Declaration> {
+        match decl {
+            Decl::Binding(b) => self.extract_names(&b.pattern),
+            _ => Vec::new()
         }
     }
-}
 
-fn find_declarations(decl: &Decl) -> Vec<Declaration> {
-    match decl {
-        Decl::Binding(b) => extract_names(&b.pattern),
-        _ => Vec::new()
+    fn extract_names(&mut self, pattern: &Pattern) -> Vec<Declaration> {
+        match pattern {
+            Pattern::Name(np) => vec!(self.make_declaration(Rc::clone(np))),
+            Pattern::Type(_, inners) => inners.iter().flat_map(|p| self.extract_names(p)).collect(),
+            Pattern::List(inners) => inners.iter().flat_map(|p| self.extract_names(p)).collect(),
+            _ => Vec::new()
+        }
     }
-}
 
-fn extract_names(pattern: &Pattern) -> Vec<Declaration> {
-    match pattern {
-        Pattern::Name(np) => vec!(Declaration(Rc::clone(np))),
-        Pattern::Type(_, inners) => inners.iter().flat_map(|p| extract_names(p)).collect(),
-        Pattern::List(inners) => inners.iter().flat_map(|p| extract_names(p)).collect(),
-        _ => Vec::new()
+    fn make_declaration(&mut self, name: Rc<NamePattern>) -> Declaration {
+        let id = self.declaration_id_seq;
+        self.declaration_id_seq += 1;
+        Declaration {
+            name,
+            id
+        }
+    }
+
+    fn add_declarations(&mut self, declarations: &Vec<Declaration>) {
+        for d in declarations {
+            self.declarations.insert(Rc::clone(&d.name), d.id);
+        }
     }
 }
 
